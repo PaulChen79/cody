fc81308486794b14074e64a755a51a2ae4fe5cc7
PaulChen79 <paul791111@gmail.com>
2024-10-22 16:33:21 +0800

    fix: 🐛 asdasdasd

--- internal/gin/handler/test.go
+++ internal/gin/handler/test.go
@@ -12,3 +12,39 @@ func Hello() (int, error) {
 func TestFunc(a, b int) (int, error) {
 	return a + b, nil
 }
+
+func TestFunc1(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc2(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc3(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc4(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc5(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc6(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc7(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc8(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func TestFunc9(a, b int) (int, error) {
+	return a + b, nil
+}
